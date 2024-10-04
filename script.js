e7719a066ac0b9c119b90352f971622f3fd61bb0
grant <grant.enderkids@gmail.com>
2024-10-03 22:14:04 -0600

    added the event listener for the labelBalance

--- script.js
+++ script.js
@@ -606,4 +606,35 @@ console.log(movements);
 // });
 movements.sort((a, b) => b - a);
 console.log(movements);
+
+
+//Array. From
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+const x = new Array(7);
+console.log(x);
+//console.log(x.map(() => 5));
+// x.fill(1);
+x.fill(1, 3, 5);
+x.fill(1);
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
+
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
 */
+
+labelBalance.addEventListener('click', function () {
+  console.log('labelBalance clicked!');
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+  console.log(movementsUI);
+});
+console.log(labelBalance);
